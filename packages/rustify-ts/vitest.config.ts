6e5ac88cf9e60ea683e0db66f092f04007b7fedc
Angelo Zambrano <angelozam17@gmail.com>
2026-07-24 08:39:41 -0500

    chore: scaffold pnpm monorepo with rustify-ts v2 package

---
+++ packages/rustify-ts/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['test/**/*.test.ts'],
+    passWithNoTests: true,
+    typecheck: {
+      enabled: true,
+      include: ['test/**/*.test-d.ts'],
+      tsconfig: './tsconfig.json',
+    },
+  },
+})
