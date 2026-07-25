b9b7b0c5dc9dd5229cb6724d6b25983266916327
Angelo Zambrano <angelozam17@gmail.com>
2026-07-25 17:26:00 -0500

    feat(eslint-plugin-rustify): assemble the flat-config plugin object

--- packages/eslint-plugin-rustify/src/index.ts
+++ packages/eslint-plugin-rustify/src/index.ts
@@ -1,1 +1,15 @@
-export {}
+import { noFloatingOutcome } from './rules/no-floating-outcome'
+
+const plugin = {
+  meta: { name: 'eslint-plugin-rustify', version: '0.1.0-alpha.0' },
+  rules: { 'no-floating-outcome': noFloatingOutcome },
+  configs: {} as { recommended: unknown },
+}
+
+plugin.configs.recommended = {
+  name: 'rustify/recommended',
+  plugins: { rustify: plugin },
+  rules: { 'rustify/no-floating-outcome': 'error' },
+}
+
+export default plugin
